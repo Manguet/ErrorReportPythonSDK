1991206a212a95f5df0da34daad5a4398c6a3dc2
Benjamin <benjamin.manguet@gmail.com>
2025-08-12 17:53:30 +0200

    V1.0.1 VueJ SDK

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="error-explorer-python",
-    version="1.0.0",
+    version="1.0.1",
     author="Manguet",
     author_email="benjamin.manguet@gmail.com",
     description="Python SDK for Error Explorer - Capture and report errors automatically",
